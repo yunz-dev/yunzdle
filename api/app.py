f1a15b4e53ce8a87750b2e8f1012a3198a031cd1
Yunus <yunz@Yunz-Book.local>
2024-11-16 21:44:28 +1100

    ADDED: database connection

--- api/app.py
+++ api/app.py
@@ -1,6 +1,14 @@
 # app.py
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 from fastapi import FastAPI
 
+scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+client = gspread.authorize(creds)
+sheet = client.open("Yunzdle Database").sheet1
+data = sheet.get_all_records()
+print(data)
 app = FastAPI()
 
 @app.get("/")
